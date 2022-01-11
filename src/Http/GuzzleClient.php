63d123c5e2060ba660098b7c27f71571b5decf81
Daniel Zambo <zambodaniel@gmail.com>
2022-01-11 13:57:59 +0100

    Remove custom http client logic

--- src/Http/GuzzleClient.php
+++ src/Http/GuzzleClient.php
@@ -5,13 +5,5 @@ use Psr\Http\Message\RequestInterface;
 
 class GuzzleClient extends \Omnipay\Common\Http\Client
 {
-    /**
-     * @inheritdoc
-     */
-    public function sendRequest(RequestInterface $request)
-    {
-        return $this->request()->send($request, [
-            'verify' => __DIR__ . '/../certs/cacert.pem'
-        ]);
-    }
+    //
 }
